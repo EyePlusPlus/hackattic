62efbaec26e439ca1666e7408ac8d853b7694878
Mansi Shah <shah.mansi246@gmail.com>
2025-10-02 12:48:35 -0400

    Make it cleaner

--- help_me_unpack/main.go
+++ help_me_unpack/main.go
@@ -24,83 +24,115 @@ type SolutionBody struct {
 	BigEndianDouble float64 `json:"big_endian_double"`
 }
 
-func main() {
+var url = "https://hackattic.com/challenges/help_me_unpack/%s?access_token=%s"
 
-	url := fmt.Sprintf("https://hackattic.com/challenges/help_me_unpack/problem?access_token=%s", os.Getenv("ACCESS_TOKEN"))
-	res, err := http.Get(url)
-	if err != nil {
-		panic("Get failed")
-	}
-
-	respBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-
+func fetchProblem() (JSONBody, error) {
 	var jsonBody JSONBody
-	if err := json.Unmarshal(respBody, &jsonBody); err != nil {
-		panic(err)
-	}
+	res, err := http.Get(fmt.Sprintf(url, "problem", os.Getenv("ACCESS_TOKEN")))
 
-	decoded, err := base64.StdEncoding.DecodeString(jsonBody.Bytes)
 	if err != nil {
-		panic(err)
+		return jsonBody, fmt.Errorf("help_me_unpack#fetchProblem: %w", err)
 	}
 
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return jsonBody, fmt.Errorf("help_me_unpack#fetchProblem: %w", err)
+	}
+
+	if err := json.Unmarshal(resBody, &jsonBody); err != nil {
+		return jsonBody, fmt.Errorf("help_me_unpack#fetchProblem: %w", err)
+	}
+
+	return jsonBody, nil
+}
+
+func unpackSolutionBytes(decoded []byte) (SolutionBody, error) {
 	var solution SolutionBody
 
 	reader := bytes.NewReader(decoded)
 	readerErr := binary.Read(reader, binary.LittleEndian, &solution.Int)
 	if readerErr != nil {
-		panic(readerErr)
+		return SolutionBody{}, readerErr
 	}
 	readerErr = binary.Read(reader, binary.LittleEndian, &solution.Uint)
 	if readerErr != nil {
-		panic(readerErr)
+		return SolutionBody{}, readerErr
 	}
 	readerErr = binary.Read(reader, binary.LittleEndian, &solution.Short)
 	if readerErr != nil {
-		panic(readerErr)
+		return SolutionBody{}, readerErr
 	}
 	_, seekErr := reader.Seek(2, io.SeekCurrent)
 	if seekErr != nil {
-		panic(seekErr)
+		return SolutionBody{}, seekErr
 	}
 	readerErr = binary.Read(reader, binary.LittleEndian, &solution.Float)
 	if readerErr != nil {
-		panic(readerErr)
+		return SolutionBody{}, readerErr
 	}
 	readerErr = binary.Read(reader, binary.LittleEndian, &solution.Double)
 	if readerErr != nil {
-		panic(readerErr)
+		return SolutionBody{}, readerErr
 	}
 	readerErr = binary.Read(reader, binary.BigEndian, &solution.BigEndianDouble)
 	if readerErr != nil {
-		panic(readerErr)
+		return SolutionBody{}, readerErr
 	}
 
-	solRes, err := json.Marshal(solution)
+	return solution, nil
+}
+
+func submitSolution(solution SolutionBody) (map[string]interface{}, error) {
+	jsonBody, err := json.Marshal(solution)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	url = fmt.Sprintf("https://hackattic.com/challenges/help_me_unpack/solve?access_token=%s", os.Getenv("ACCESS_TOKEN"))
-	verRes, err := http.Post(url, "application/json", bytes.NewReader(solRes))
+	submitUrl := fmt.Sprintf(url, "solve", os.Getenv("ACCESS_TOKEN"))
+	response, err := http.Post(submitUrl, "application/json", bytes.NewReader(jsonBody))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	verResBody, err := io.ReadAll(verRes.Body)
+	defer response.Body.Close()
+	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	var verJson map[string]interface{}
+	var jsonResponse map[string]interface{}
 
-	if err := json.Unmarshal(verResBody, &verJson); err != nil {
-		panic(err)
+	if err := json.Unmarshal(responseBody, &jsonResponse); err != nil {
+		return nil, err
 	}
 
-	fmt.Println(verJson)
+	return jsonResponse, nil
+}
+
+func main() {
+
+	problemJson, fetchErr := fetchProblem()
+	if fetchErr != nil {
+		panic(fetchErr)
+	}
+
+	decoded, decodeErr := base64.StdEncoding.DecodeString(problemJson.Bytes)
+	if decodeErr != nil {
+		panic(decodeErr)
+	}
+
+	solution, unpackErr := unpackSolutionBytes(decoded)
+	if unpackErr != nil {
+		panic(unpackErr)
+	}
+
+	submitResponseJson, submitErr := submitSolution(solution)
+	if submitErr != nil {
+		panic(submitErr)
+	}
+
+	fmt.Println(submitResponseJson)
 
 }
